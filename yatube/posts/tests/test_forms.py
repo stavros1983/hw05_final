79caeb6a1a556609b018b1791c613b91dff71d15
Бабаян Армен <stavros1983@yandex.ru>
2023-03-01 01:01:51 +0400

    after first review

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.core.cache import cache
+from django.core.files.uploadedfile import SimpleUploadedFile
 from ..models import Group, Post, Comment
 
 User = get_user_model()
@@ -27,6 +29,7 @@ class PostFormTests(TestCase):
         self.authorized_user.force_login(self.post_author)
         self.auth_user_comment = Client()
         self.auth_user_comment.force_login(self.comment_author)
+        cache.clear()
 
     def test_authorized_user_create_comment(self):
         """Проверка создания коментария авторизированным клиентом."""
@@ -71,9 +74,23 @@ class PostFormTests(TestCase):
     def test_authorized_user_create_post(self):
         """Проверка создания записи авторизированным клиентом."""
         posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {
             'text': 'Текст поста',
             'group': self.group.id,
+            'image': uploaded,
         }
         response = self.authorized_user.post(
             reverse('posts:create'),
@@ -91,6 +108,7 @@ class PostFormTests(TestCase):
         self.assertEqual(post.text, form_data['text'])
         self.assertEqual(post.author, self.post_author)
         self.assertEqual(post.group_id, form_data['group'])
+        self.assertEqual(post.image.name, 'posts/small.gif')
 
     def test_authorized_user_edit_post(self):
         """Проверка редактирования записи авторизированным клиентом."""
