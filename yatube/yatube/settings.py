df22dc830f8ebb816d2d891a82956fcf87dd3261
Бабаян Армен <stavros1983@yandex.ru>
2023-03-26 06:14:47 +0400

    after sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -30,6 +30,8 @@ handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
